aefa6fb4e53a578331ab2c6ae8d5e1dec4df2277
caudiopd <86140460+caudiopd@users.noreply.github.com>
2021-06-19 11:29:55 +0530

    Add files via upload

---
+++ test_.py
@@ -0,0 +1,28 @@
+import unittest
+
+from app import app
+
+class BasicTestCase(unittest.TestCase):
+    
+    def test_login(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_profile(self):
+        tester = app.test_client(self)
+        response = tester.get('profile.html', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('index.html', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_predict(self):
+        tester = app.test_client(self)
+        response = tester.get('y_predict', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
